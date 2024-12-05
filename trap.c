5199496860c804bfa3988c8952b31a495ffa6c26
Tomer Rozen <tomerrzn@gmail.com>
2024-12-05 17:25:33 +0200

    fix logging

--- trap.c
+++ trap.c
@@ -58,7 +58,7 @@ trap(struct trapframe *tf)
     // Increment the tick counter for the currently running process
     if (myproc() && myproc()->state == RUNNING) {
       myproc()->ticks++; // Increment the ticks counter
-      cprintf('Process %d ticks is %d\n', myproc()->pid, myproc()->ticks);
+      cprintf("Process %d ticks is %d\n", myproc()->pid, myproc()->ticks);
 
       // Check if the process has reached the 10-tick time slice
       if (myproc()->ticks >= 10) {
