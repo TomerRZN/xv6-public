353bd35a9d66afe30ae0bac95caa61707e0c9642
Tomer Rozen <tomerrzn@gmail.com>
2024-12-06 15:00:38 +0200

    change sleep value

--- prioritytest.c
+++ prioritytest.c
@@ -5,17 +5,18 @@
 #define NUM_PROCESSES 3  // Number of child processes
 
 int main() {
-  int pid;
+  setpriority(getpid(), 1);
 
+  int pid;
   // Create NUM_PROCESSES child processes
   for (int i = 0; i < NUM_PROCESSES; i++) {
     pid = fork();
     if (pid == 0) {
       // Child process: set priority and run workload
-      int priority = NUM_PROCESSES - i;  // Assign priorities (e.g., 5, 4, 3, 2, 1)
-      setpriority(getpid(), priority);  // Set priority using system call
-      sleep(5);
-      exit();  // Exit after completing workload
+      int priority = NUM_PROCESSES - i + 1;
+      setpriority(getpid(), priority);
+      sleep(1000);
+      exit();
     }
   }
 
