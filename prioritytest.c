683216dee1659bc0639a786f556c445710b8aa39
Tomer Rozen <tomerrzn@gmail.com>
2024-12-06 15:17:23 +0200

    simpler test

--- prioritytest.c
+++ prioritytest.c
@@ -2,40 +2,23 @@
 #include "stat.h"
 #include "user.h"
 
-#define NUM_PROCESSES 3  // Number of child processes
-#define ITERATIONS 50    // Number of iterations for workload (to generate scheduler logs)
-
-void workload() {
-  for (int i = 0; i < ITERATIONS; i++) {
-    // Do some work (no printing)
-    asm volatile("");  // Prevent compiler optimizations
-  }
-}
 
 int main() {
   setpriority(getpid(), 1);  // Set parent process priority to 1
 
-  int pid;
-  int priorities[NUM_PROCESSES] = {2, 3, 4};  // Priorities for child processes
+  int pid = fork();
 
-  // Create child processes
-  for (int i = 0; i < NUM_PROCESSES; i++) {
-    pid = fork();
-    if (pid == 0) {
-      // Child process
-      setpriority(getpid(), priorities[i]);  // Set child process priority
-      workload();  // Run workload
-      exit();  // Exit after workload completes
-    }
-  }
+  // Child process
+  if (pid == 0) {
+    setpriority(getpid(), 2);  // Set child process priority
+    int count = 0;
+    while(count < 100000);
+        count++;
 
-  // Parent process: lower its own priority after creating children
-  setpriority(getpid(), 5);
-
-  // Wait for child processes to finish
-  for (int i = 0; i < NUM_PROCESSES; i++) {
+  // Parent process
+  else {
+    setpriority(getpid(), 3); // Let the child finish first
     wait();
   }
-
   exit();
 }
