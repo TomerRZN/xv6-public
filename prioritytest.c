f4504ab2a0335555f8a0a5a152077e2ab0f734fc
Tomer Rozen <tomerrzn@gmail.com>
2024-12-06 14:53:51 +0200

    change test

--- prioritytest.c
+++ prioritytest.c
@@ -2,12 +2,11 @@
 #include "stat.h"
 #include "user.h"
 
-#define NUM_PROCESSES 5  // Number of child processes
+#define NUM_PROCESSES 3  // Number of child processes
 
 void workload(int id, int priority) {
   for (int i = 0; i < 10; i++) {
-    printf(1, "Process %d [Priority %d]: iteration %d\n", id, priority, i);
-    sleep(10);  // Sleep to make scheduling observable
+    sleep(5);  // Sleep to make scheduling observable
   }
 }
 
@@ -31,6 +30,5 @@ int main() {
     wait();
   }
 
-  printf(1, "Priority scheduling test completed.\n");
   exit();
 }
